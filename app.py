74aa0a374fb7046cd89ac22eb922bfe379e0a885
Kristania-Wijaya <kristaniamartha@gmail.com>
2026-01-05 17:54:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.write("STREAMLIT JALAN")
+st.set_page_config(
+    page_title="Input Data",
+    layout="centered"
+)
+
+st.title("Aplikasi Input Data")
+st.write("Aplikasi siap digunakan.")
+st.subheader("Form Input Data")
+
+nama = st.text_input("Nama Lengkap")
+nik = st.text_input("NIK")
+
+if st.button("Simpan"):
+    st.success(f"Data {nama} dengan NIK {nik} berhasil disimpan (sementara).")
